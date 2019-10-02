a179e19a8e7b1e40a514d344b1e6fc637ca7b025
federicop <federicopozzana@hotmail.it>
2019-10-02 10:23:45 +0200

    no clk and bypass on RF read/write & fix DLX

--- DLX-components/RegisterFile/registerfile.vhd
+++ DLX-components/RegisterFile/registerfile.vhd
@@ -12,10 +12,7 @@ entity REGISTER_FILE is
 	         numBit_registers : integer := NumBitRegisterFile);
   port (CK :	IN std_logic;
         Reset : IN std_logic;
-	      Enable :	IN std_logic;
-	      Write : IN std_logic;
-	      Read_one :	IN std_logic;
-	      Read_two :	IN std_logic;
+	      Write_enable : IN std_logic;
 	      Write_address : IN std_logic_vector(numBit_address-1 downto 0);
  	      Read_one_address :	IN std_logic_vector(numBit_address-1 downto 0);
  	      Read_two_address : IN std_logic_vector(numBit_address-1 downto 0);
@@ -30,41 +27,35 @@ architecture BEHAVIORAL of REGISTER_FILE is
 	signal REGISTERS : REG_ARRAY := (others => (others => '0')); --initialize my registers to 0
 
   begin
-    rf_process : process (CK)
+    write : process(CK,Reset,Write_enable,Write_address,Data_in)
+      begin
+        if(Reset = '1') then
+          REGISTERS <= (others => (others => '0'));
+        elsif(CK'event and CK = '1' and Write_enable = '1') then
+          if(not(to_integer(unsigned(Write_address)) = 0)) then
+            REGISTERS(to_integer(unsigned(Write_address))) <= Data_in;
+          end if;
+        end if;
+      end process;
+
+    read : process (Read_one_address, Read_two_address,Write_enable,Data_in,Write_address)
 	   begin
-		     if CK'event and CK='1' then
-			     if (RESET = '1') then
-				     REGISTERS <= (others => (others => '0'));
-				     Data_one_out <= (others => '0');
-				     Data_two_out <= (others => '0');
-			     elsif (ENABLE = '1') then
-				     if (Write='1' and Read_one='1' and Read_two='1') then
-               data_one_out <= REGISTERS(to_integer(unsigned(Read_one_address)));
-					     data_two_out <= REGISTERS(to_integer(unsigned(Read_two_address)));
-					     REGISTERS(to_integer(unsigned(Write_address))) <= Data_in;
-				     end if;
-				     if (Write='1' and Read_one='1' and Read_two='0') then
-               data_one_out <= REGISTERS(to_integer(unsigned(Read_one_address)));
-					     REGISTERS(to_integer(unsigned(Write_address))) <= Data_in;
-				     end if;
-				     if (Write='1' and Read_one='0' and Read_two='1') then
-               data_two_out <= REGISTERS(to_integer(unsigned(Read_two_address)));
-               REGISTERS(to_integer(unsigned(Write_address))) <= Data_in;
-				     end if;
-				     if (Write='0' and Read_one='1' and Read_two='1') then
-               data_one_out <= REGISTERS(to_integer(unsigned(Read_one_address)));
-               data_two_out <= REGISTERS(to_integer(unsigned(Read_two_address)));
-				     end if;
-				     if (Write='0' and Read_one='0' and Read_two='1') then
-               data_two_out <= REGISTERS(to_integer(unsigned(Read_two_address)));
-				     end if;
-				     if (Write='0' and Read_one='1' and Read_two='0') then
-               data_one_out <= REGISTERS(to_integer(unsigned(Read_one_address)));
-				     end if;
-			     end if;
-		     end if;
-       end process;
-     end BEHAVIORAL;
+       if(Write_enable = '1' and Read_one_address = Write_address) then
+         if(not(to_integer(unsigned(Write_address)) = 0)) then
+           data_one_out <= Data_in;
+         end if;
+        data_two_out <= REGISTERS(to_integer(unsigned(Read_two_address)));
+      elsif(Write_enable = '1' and Read_two_address = Write_address) then
+        if(not(to_integer(unsigned(Write_address)) = 0)) then
+          data_two_out <= Data_in;
+        end if;
+        data_one_out <= REGISTERS(to_integer(unsigned(Read_one_address)));
+      else
+        data_one_out <= REGISTERS(to_integer(unsigned(Read_one_address)));
+        data_two_out <= REGISTERS(to_integer(unsigned(Read_two_address)));
+      end if;
+     end process;
+   end BEHAVIORAL;
 
 configuration CFG_REGISTER_FILE_BEHAVIORAL of REGISTER_FILE is
   for BEHAVIORAL
