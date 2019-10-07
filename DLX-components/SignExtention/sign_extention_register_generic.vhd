eedca744a2a203719a3ceb56f5c3b11776f82782
federicop <federicopozzana@hotmail.it>
2019-10-07 22:52:25 +0200

    Revert "Revert "dlx update""
    This reverts commit 3b179742f3cb6ec849f3fb0880945bcae52beb06.

--- DLX-components/SignExtention/sign_extention_register_generic.vhd
+++ DLX-components/SignExtention/sign_extention_register_generic.vhd
@@ -20,7 +20,11 @@ architecture BHEAVIORAL of SIGN_EXTENTION is
   begin
     process(D)
       begin
-        Q <= "0000000000000000" & D;
+        if(D(15) = '0') then
+          Q <= "0000000000000000" & D;
+        elsif(D(15) = '1') then
+          Q <= "1111111111111111" & D;
+        end if;
       end process;
 
 end BHEAVIORAL;
