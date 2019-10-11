27e94b3053b60502cb86033b20322277fde93a02
federicop <federicopozzana@hotmail.it>
2019-10-11 11:37:13 +0200

    update test.asm

--- DLX-components/Memory/Instruction_memory/test.asm
+++ DLX-components/Memory/Instruction_memory/test.asm
@@ -1,10 +1,16 @@
 addi r1, r0, 100
-ciclo:
-subi r1, r1, 10
-addi r3, r3, 10
-add r4,r3,r1
-bnez r1, ciclo
+nop
+nop
+nop
+sw 1(r0),r1
+nop
+nop
+nop
+lw r3,1(r0)
+nop
+nop
+nop
+addi r4,r3, 5
 end:
 j end
-nop
 
